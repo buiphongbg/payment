d8715db2453912d0b9820c068678b4a593e54bef
phongbui <buiphongbg@gmail.com>
2017-11-16 15:29:31 +0700

    Add config file

---
+++ config/payment.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'driver' => env('PAYMENT_DRIVER', 'napas'),
+
+    'napas' => [
+        'merchant_id' => env('NAPAS_MERCHANT_ID'),
+        'secure_hash' => env('NAPAS_SECURE_HASH'),
+        'access_code' => env('NAPAS_ACCESS_CODE')
+    ],
+
+    'payoo' => [
+        'key' => env('PAYOO_KEY'),
+        'secret' => env('PAYOO_SECRET'),
+    ]
+];
