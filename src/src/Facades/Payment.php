aaaf21b5c0a94b7835eeadd24aca253c80ca1e87
phongbui <buiphongbg@gmail.com>
2017-11-16 14:56:15 +0700

    Update payment

---
+++ src/src/Facades/Payment.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by Phong Bui.
+ * Date: 16/11/2017
+ * Time: 13:52
+ */
+
+namespace PhongBui\Payment\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * Class Payment Facade
+ *
+ * @method static getPaymentUrl($orderId, $amount, $successUrl, $failedUrl, $cancelUrl)
+ * @package PhongBui\Payment\Facades
+ */
+class Payment extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'payment';
+    }
+}
